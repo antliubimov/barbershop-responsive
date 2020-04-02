7a70e8be80565bc9e0648188baab3823919df7a2
Anton Liubimov <ant.liubimov@gmail.com>
2020-04-02 19:51:07 +0500

    done nav

--- gulp/tasks/script.js
+++ gulp/tasks/script.js
@@ -3,12 +3,16 @@
 /* Запускаем минификацию js */
 
 const gulp = require("gulp");
+const babel = require("gulp-babel");
 const minifyjs = require("gulp-uglify");
 const rename = require("gulp-rename");
 const server = require("browser-sync").create();
 
 module.exports = function script() {
   return gulp.src("source/js/script.js")
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
     .pipe(minifyjs())
     .pipe(gulp.dest("build/js"))
     .pipe(rename("script.min.js"))
